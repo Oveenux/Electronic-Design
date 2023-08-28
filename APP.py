ea8abd634a5e59f2128658c497df0a062dea2243
Oveenux <114117291+Oveenux@users.noreply.github.com>
2023-08-27 19:34:17 -0500

    Add files via upload

--- APP.py
+++ APP.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template,request, Response
 import socket
 import time
 
+host = "0.0.0.0"  # Standard loopback interface address (localhost)
+port = 9001 # Port to listen on (non-privileged ports are > 1023)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((host, port))
+print(f"UDP server listening on {host}:{port}")
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,12 +15,6 @@ def index():
     return render_template('PRUEBA.html')
 
 def enviar_datos():
-    
-    host = "0.0.0.0"  # Standard loopback interface address (localhost)
-    port = 9001 # Port to listen on (non-privileged ports are > 1023)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind((host, port))
-    print(f"UDP server listening on {host}:{port}")
 
     while True:
         
@@ -31,4 +31,4 @@ def stream():
     return Response(enviar_datos(), content_type='text/event-stream')
 
 if __name__ == '__main__':
-    app.run()
+    app.run("192.168.1.14", port=5000,)
