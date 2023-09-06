073bc7526b6eaf006f1fd8704f37f1de3cf688fc
Oveenux <114117291+Oveenux@users.noreply.github.com>
2023-09-06 11:07:09 -0500

    Add files via upload

--- APP.py
+++ APP.py
@@ -1,34 +1,51 @@
 from flask import Flask, render_template,request, Response
-import socket
 import time
+import mysql.connector
 
-host = "0.0.0.0"  # Standard loopback interface address (localhost)
-port = 9001 # Port to listen on (non-privileged ports are > 1023)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((host, port))
-print(f"UDP server listening on {host}:{port}")
+# Configura la conexión a la base de datos
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('PRUEBA.html')
+    return render_template('PRUEBA2.html')
+
+
 
 def enviar_datos():
 
     while True:
+        conexion = mysql.connector.connect(
+            host = "locationdb.col4pixfadqv.us-east-2.rds.amazonaws.com",
+            user = "root",
+            password= "123456789",
+            database= "LocationDB"
+        )
+
+        cursor = conexion.cursor()
+        # Ejecuta una consulta SQL para obtener el último dato agregado
+        cursor.execute("SELECT Latitud, Longitud, Estampa_de_tiempo FROM gps ORDER BY id DESC LIMIT 1")
+
+        # Recupera el último dato agregado en ambas columnas
+        ultima_fila = cursor.fetchone()
+
+        # Imprime el resultado (esto puede variar dependiendo de tus columnas)
+        #print("Último dato en columna1:", ultima_fila[0])
+        #print("Último dato en columna2:", ultima_fila[1])
+        #print("Último dato en columna3:", ultima_fila[2])
         
-        data, address = server_socket.recvfrom(1024)
-        IP , puerto = address
-        print(f'Received from {address}')
-        print("="*40)
-        nuevo_dato = f'ip:{IP}: ' + str(data.decode())  # Datos nuevos simulados
+        nuevo_dato = f'{ultima_fila[0]}, {ultima_fila[1]},{ultima_fila[2]}'  # Datos enviados
         yield f'data: {nuevo_dato}\n\n'
         time.sleep(1)  # Controla la frecuencia de envío de datos
+        conexion.close()
+        stream()
+        
+
         
 @app.route('/stream')
 def stream():
     return Response(enviar_datos(), content_type='text/event-stream')
 
 if __name__ == '__main__':
-    app.run("192.168.1.14", port=5000,)
+    app.run( port=5000)
